09a6f7c9ac9f0af828619579e34c62f41237012d
Anuj Kulkarni <anuj.stonecold@gmail.com>
2013-07-09 21:40:23 +0530

    Sepearated the package array_strings to array and strings

---
+++ src/com/anuj/interview/practice/strings/StringUnique.java
@@ -0,0 +1,26 @@
+package com.anuj.interview.practice.strings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StringUnique {
+	public static void main(String[] args) throws IOException {
+		System.out.println("Enter String : ");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String string = reader.readLine();
+		if (checkUnique(string)) {
+			System.out.println(string + " has all unique characters");
+		} else {
+			System.out.println(string + " does not have unique characters");
+		}
+	}
+
+	private static boolean checkUnique(String string) {
+		String newString = "";
+		for(int i = 0; i < string.length(); i++) {
+			
+		}
+		return false;
+	}
+}
