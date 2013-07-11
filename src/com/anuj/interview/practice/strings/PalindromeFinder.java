35a193da36cf2af266cf4923516dd82a4a47680b
Anuj Kulkarni <anuj.stonecold@gmail.com>
2013-07-11 09:39:54 +0530

    Updated version of code for Palindrome Finder Removed String reverse and added character check to determine palindromicity

--- src/com/anuj/interview/practice/strings/PalindromeFinder.java
+++ src/com/anuj/interview/practice/strings/PalindromeFinder.java
@@ -3,25 +3,30 @@ package com.anuj.interview.practice.strings;
 import java.io.*;
 
 public class PalindromeFinder {
-	public static boolean isPalindrome(String word){
-		return word.equals(reverse(word));
+	public static boolean iPalindrome(String word1, String word2) {
+		if (word1.length() != word2.length()) {
+			return false;
+		} else {
+			for (int i = 0; i < word1.length(); i++) {
+				if (!Character.toString(word1.charAt(i)).equals(
+				  Character.toString(word2.charAt(word2.length() - 1 - i)))) { return false; }
+			}
+			return true;
+		}
 	}
 
-	public static String reverse(String word) {
-		StringBuilder reverse = new StringBuilder();
-		for(int i = word.length() -1; i >= 0; i --) {
-			reverse.append(Character.toString(word.charAt(i)));
-		}
-		return reverse.toString();
-	}
-	public static void main(String[] args) throws IOException{
-		System.out.println("Enter word : ");
+	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String word = reader.readLine();
-		if(isPalindrome(word)) {
-			System.out.println(word + " is Palindrome");
+		System.out.println("Enter word1 : ");
+		String word1 = reader.readLine();
+		System.out.println("Enter word2 : ");
+		String word2 = reader.readLine();
+		if (iPalindrome(word1, word2)) {
+			System.out
+			  .println(word1 + " and " + word2 + " are palindrome of each other");
 		} else {
-			System.out.println(word + " is not Palindrome");
+			System.out.println(word1 + " and " + word2
+			  + " are not palindrome of each other");
 		}
 	}
 }
