09085274273b272b691f6e4f142f19c6c40e2da6
Anuj Kulkarni <anuj.stonecold@gmail.com>
2013-07-11 21:42:55 +0530

    Modified string rotation program to change method names

--- src/com/anuj/interview/practice/strings/StringRotation.java
+++ src/com/anuj/interview/practice/strings/StringRotation.java
@@ -3,16 +3,14 @@ package com.anuj.interview.practice.strings;
 import java.io.*;
 
 public class StringRotation {
-	public static boolean isSubString(String word1, String word2) {
+	public static boolean isRotation(String word1, String word2) {
 		if (word1.length() != word2.length()) {
 			return false;
 		} else {
 			char c = word1.charAt(0);
 			int i;
 			for (i = 0; i < word2.length(); i++) {
-				if (word2.charAt(i) == c) {
-					break;
-				}
+				if (word2.charAt(i) == c) break;
 			}
 			return word1.equals(word2.substring(i, word2.length())
 			  + word2.substring(0, i));
@@ -27,7 +25,7 @@ public class StringRotation {
 		String word1 = reader.readLine();
 		System.out.println("Enter second word : ");
 		String word2 = reader.readLine();
-		if (isSubString(word1, word2)) {
+		if (isRotation(word1, word2)) {
 			System.out.println(word2 + " is rotation of " + word1);
 		} else {
 			System.out.println(word2 + " is not a rotation of " + word1);
