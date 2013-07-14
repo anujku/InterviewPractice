cef9c481bf9b29488d1acdd6feb0788f415f82b3
Anuj Kulkarni <anuj.stonecold@gmail.com>
2013-07-14 22:55:01 +0530

    Added code for BubbleSort and Selection Sort

---
+++ src/com/anuj/interview/practice/sorting/BubbleSort.java
@@ -0,0 +1,51 @@
+package com.anuj.interview.practice.sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// Bubble sort, sometimes incorrectly referred to as sinking sort, is a simple
+// sorting algorithm that works by repeatedly stepping through the list to be
+// sorted, comparing each pair of adjacent items and swapping them if they are
+// in the wrong order. The pass through the list is repeated until no swaps are
+// needed, which indicates that the list is sorted. The algorithm gets its name
+// from the way smaller elements "bubble" to the top of the list. Because it
+// only uses comparisons to operate on elements, it is a comparison sort.
+// Although the algorithm is simple, most of the other sorting algorithms are
+// more efficient for large lists.
+public class BubbleSort {
+	// Complexity : O(N^2)
+	// Worst case performance O(n^2)
+	// Best case performance O(n)
+	// Average case performance O(n^2)
+	// Worst case space complexity O(1) auxiliary
+	private static int[] bubbleSort(int[] array) {
+		for (int outer = array.length - 1; outer > 1; outer--) {
+			for (int inner = 0; inner < outer; inner++) {
+				if (array[inner] > array[inner + 1]) {
+					int temp = array[inner];
+					array[inner] = array[inner + 1];
+					array[inner + 1] = temp;
+				}
+			}
+		}
+		return array;
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Enter size of the array to bubble sort : ");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(reader.readLine());
+		int[] array = new int[size];
+		System.out.println("Enter the array to bubble sort : ");
+		for (int i = 0; i < array.length; i++) {
+			System.out.println("Enter " + i + "th element of the array");
+			array[i] = Integer.parseInt(reader.readLine());
+		}
+		array = bubbleSort(array);
+		System.out.println("Bubble sorted array : ");
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+}
---
+++ src/com/anuj/interview/practice/sorting/SelectionSort.java
@@ -0,0 +1,59 @@
+package com.anuj.interview.practice.sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// The algorithm divides the input list into two parts: the sublist of items
+// already sorted, which is built up from left to right at the front (left) of
+// the list, and the sublist of items remaining to be sorted that occupy the
+// rest of the list. Initially, the sorted sublist is empty and the unsorted
+// sublist is the entire input list. The algorithm proceeds by finding the
+// smallest (or largest, depending on sorting order) element in the unsorted
+// sublist, exchanging it with the leftmost unsorted element (putting it in
+// sorted order), and moving the sublist boundaries one element to the right.
+public class SelectionSort {
+	// Worst case performance О(n2)
+	// Best case performance О(n2)
+	// Average case performance О(n2)
+	// Worst case space complexity О(n) total, O(1) auxiliary
+	private static int[] selctionSort(int[] array) {
+		for (int outer = 0; outer < array.length - 1; outer++) {
+			int minPos = findMinPos(array, outer);
+			int temp = array[outer];
+			array[outer] = array[minPos];
+			array[minPos] = temp;
+		}
+		return array;
+	}
+
+	private static int findMinPos(int[] array, int startIndex) {
+		int minIndex = startIndex;
+		int min = array[minIndex];
+		
+		for (int i = startIndex; i < array.length; i++) {
+			if (array[i] < min) {
+				min = array[i];
+				minIndex = i;
+			}
+		}
+		return minIndex;
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Enter size of the array to do selction sort : ");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(reader.readLine());
+		int[] array = new int[size];
+		System.out.println("Enter the array to do selction sort : ");
+		for (int i = 0; i < array.length; i++) {
+			System.out.println("Enter " + i + "th element of the array");
+			array[i] = Integer.parseInt(reader.readLine());
+		}
+		array = selctionSort(array);
+		System.out.println("Selection sorted array : ");
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+}
