549249be248dd65491a33af39a67dfde128cd323
Anuj Kulkarni <anuj.stonecold@gmail.com>
2013-07-19 10:28:27 +0530

    Added code for quick sort

--- src/com/anuj/interview/practice/sorting/advance/QuickSort.java
+++ src/com/anuj/interview/practice/sorting/advance/QuickSort.java
@@ -5,9 +5,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class QuickSort {
-	private static int[] array;
+	public static void swap(int array[], int x, int y) {
+		int temp = array[x];
+		array[x] = array[y];
+		array[y] = temp;
+	}
 
-	private static void quickSort() {
+	// Reorganizes the given list so all elements less than the first are
+	// before it and all greater elements are after it.
+	public static int partition(int array[], int f, int l) {
+		int pivot = array[f];
+		while (f < l) {
+			while (array[f] < pivot)
+				f++;
+			while (array[l] > pivot)
+				l--;
+			swap(array, f, l);
+		}
+		return f;
+	}
+
+	public static void quicksort(int array[], int f, int l) {
+		if (f >= l) return;
+		int pivot_index = partition(array, f, l);
+		quicksort(array, f, pivot_index);
+		quicksort(array, pivot_index + 1, l);
+	}
+
+	private static void quickSort(int[] array) {
+		quicksort(array, 0, array.length - 1);
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -15,14 +41,14 @@ public class QuickSort {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
 		  System.in));
 		int size = Integer.parseInt(reader.readLine());
-		array = new int[size];
+		int[] array = new int[size];
 		System.out.println("Enter the array to do quick sort : ");
 		for (int i = 0; i < array.length; i++) {
 			System.out.println("Enter " + (i + 1) + "th element of the array");
 			array[i] = Integer.parseInt(reader.readLine());
 		}
 		long startTime = System.nanoTime();
-		quickSort();
+		quickSort(array);
 		long totalTime = System.nanoTime() - startTime;
 		System.out.println("Total Runtime in nanoseconds : " + totalTime);
 		System.out.println("Quick sorted array : ");
